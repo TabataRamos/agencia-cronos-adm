af5f5cee1bcd6c95a2925ece2882a0387941d16c
stelleal <stellalleal@gmail.com>
2022-01-05 02:47:38 -0300

    Modificações em admin.html e script.js: adicionado Edit, required no input de imagem no modal, () em reportValidity. Ajuste simples de formatação
    Os ajustes de formatação foram para padronizar as aspas no js, indentação e adicionar ponto e virgula (grande maioria feita pelo Prettier)

--- script.js
+++ script.js
@@ -1,118 +1,152 @@
-'use strict'
+'use strict';
 
-//Modal - Abrir e Fechar 
+//Modal - Abrir e Fechar
 
 function abrirModal() {
-    document.getElementById('modal').style.visibility = "visible";
+	document.getElementById('modal').style.visibility = 'visible';
 }
 
 function fecharModal() {
-    clearFields()
-    document.getElementById('modal').style.visibility = "hidden";
+	clearFields();
+	document.getElementById('modal').style.visibility = 'hidden';
 }
-
 
 // CRUD - CREATE READ UPDATE DELETE
 
-//CREATE
-
+//UPDATE
 const updateCurso = (index, curso) => {
-    const dbCurso = readCurso()
-    dbCurso[index] = curso
-    setLocalStorage(dbCurso)
-}
+	const dbCurso = readCurso();
+	dbCurso[index] = curso;
+	setLocalStorage(dbCurso);
+};
 
-const readCurso = () => getLocalStorage()
+//READ
+const readCurso = () => getLocalStorage();
 
-const getLocalStorage = () => JSON.parse(localStorage.getItem('db_curso')) ?? []
-const setLocalStorage = (dbCurso) => localStorage.setItem("db_curso", JSON.stringify(dbCurso));
+//Cria e lê itens no Local Storage
+const getLocalStorage = () =>
+	JSON.parse(localStorage.getItem('db_curso')) ?? [];
+const setLocalStorage = (dbCurso) =>
+	localStorage.setItem('db_curso', JSON.stringify(dbCurso));
 
+//CREATE
 function createCurso(curso) {
-    const dbCurso = getLocalStorage()
-    dbCurso.push(curso)
-    setLocalStorage(dbCurso)
+	const dbCurso = getLocalStorage();
+	dbCurso.push(curso);
+	setLocalStorage(dbCurso);
 }
 
+//Limpa todos os campos do form
 const clearFields = () => {
-    const fields = document.querySelectorAll('.modal-field')
-    fields.forEach(field => field.value = "")
-}
+	const fields = document.querySelectorAll('.modal-field');
+	fields.forEach((field) => (field.value = ''));
+};
 
 const saveCurso = () => {
-    if (isValidFields()) {
-        const curso = {
-            nome: document.getElementById('nome').value,
-            descricao: document.getElementById('descricao').value,
-            imagem: img.src
-        }
-        createCurso(curso)
-        updateTable()
-        fecharModal()
-    }
-}
+	if (isValidFields()) {
+		const curso = {
+			nome: document.getElementById('nome').value,
+			descricao: document.getElementById('descricao').value,
+			imagem: img.src,
+		};
+		const index = document.getElementById('nome').dataset.index;
+		if (index == 'new') {
+			createCurso(curso);
+			updateTable();
+			fecharModal();
+		} else {
+			updateCurso(index, curso);
+			updateTable();
+			fecharModal();
+		}
+	}
+};
 
 const isValidFields = () => {
-    return document.getElementById('form').reportValidity
-}
+	return document.getElementById('form').reportValidity();
+};
 
 //capturar imagem
 const inputFile = document.querySelector('#imagem');
 const img = document.createElement('img');
 
 inputFile.addEventListener('change', (event) => {
-    const url = URL.createObjectURL(event.target.files[0]);
-    img.src = url;
-})
+	const url = URL.createObjectURL(event.target.files[0]);
+	img.src = url;
+});
 
-const createRow = (curso) => {
-    const newRow = document.createElement('tr')
-    newRow.innerHTML = `
+const createRow = (curso, index) => {
+	const newRow = document.createElement('tr');
+	newRow.innerHTML = `
     <td>${curso.nome}</td>
     <td><img class="img-fluid" src="${curso.imagem}" alt="Ilustração"></td>
     <td>${curso.descricao}</td>
-    <td>
+    `;
+	const BotaoEditar = function () {
+		const botaoEdita = document.createElement('button');
+		botaoEdita.innerText = 'Editar';
+		botaoEdita.classList.add('btn1', 'b');
+		botaoEdita.setAttribute('id', `edit-${index}`);
+		//botaoDeleta.addEventListener('click', funcaoEditar)
 
+		return botaoEdita;
+	};
+	const BotaoDeleta = function () {
+		const botaoDeleta = document.createElement('button');
+		botaoDeleta.innerText = 'Excluir';
+		botaoDeleta.classList.add('btn2');
+		botaoDeleta.addEventListener('click', funcaoDeletar);
 
-    </td>
-    `
-    const BotaoEditar = function(){
-        const botaoEdita = document.createElement('button')
-        botaoEdita.innerText = "Editar"
-        botaoEdita.classList.add("btn1")
-        //botaoDeleta.addEventListener('click', funcaoEditar)
-    
-        return botaoEdita
-    }
-    const BotaoDeleta = function(){
-        const botaoDeleta = document.createElement('button')
-        botaoDeleta.innerText = "Excluir"
-        botaoDeleta.classList.add("btn2")
-        botaoDeleta.addEventListener('click', funcaoDeletar)
-    
-        return botaoDeleta
-    }
-    const funcaoDeletar = function(evento){
-        const botaoDeleta = evento.target
-        const tarefaDeletada = botaoDeleta.parentElement
-        tarefaDeletada.remove()
-    }
-    newRow.appendChild(BotaoEditar())
-    newRow.appendChild(BotaoDeleta())
-    document.querySelector('#tableCurso>#corpo').appendChild(newRow)
-}
+		return botaoDeleta;
+	};
 
+	//DELETE
+	const funcaoDeletar = function (evento) {
+		const botaoDeleta = evento.target;
+		const tarefaDeletada = botaoDeleta.parentElement;
+		tarefaDeletada.remove();
+	};
+	newRow.appendChild(BotaoEditar());
+	newRow.appendChild(BotaoDeleta());
+	document.querySelector('#tableCurso>#corpo').appendChild(newRow);
+};
 
 const clearTable = () => {
-    let rows = document.querySelectorAll('#tableCurso>#corpo tr')
-    rows.forEach(row => row.parentNode.removeChild(row))
-}
+	let rows = document.querySelectorAll('#tableCurso>#corpo tr');
+	rows.forEach((row) => row.parentNode.removeChild(row));
+};
 
 const updateTable = () => {
-    const dbCurso = readCurso()
-    clearTable()
-    dbCurso.forEach(createRow)
-}
+	const dbCurso = readCurso();
+	clearTable();
+	dbCurso.forEach(createRow);
+};
+
+const fillFields = (curso) => {
+	(document.getElementById('nome').value = curso.nome),
+		(document.getElementById('descricao').value = curso.descricao),
+		(img.src = curso.imagem),
+		(document.getElementById('nome').dataset.index = curso.index);
+};
+
+const editAction = (index) => {
+	const curso = readCurso()[index];
+	curso.index = index;
+	fillFields(curso);
+	abrirModal();
+};
+
+const editCurso = (event) => {
+	if (event.target.className == 'btn1 b') {
+		const [edit, index] = event.target.id.split('-');
+
+		editAction(index);
+	}
+};
 
 //Evento
 
-document.getElementById('salvar').addEventListener('click', saveCurso)
+document.getElementById('salvar').addEventListener('click', saveCurso);
+document
+	.querySelector('#tableCurso>#corpo')
+	.addEventListener('click', editCurso);
